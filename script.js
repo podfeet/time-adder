1742abb9a01d9438e338dc58ff013f346e14cef6
NosillaCast <allison@podfeet.com>
2021-11-03 20:08:15 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+/* eslint-env jquery */
+/* global Mustache */
+
+const timeInputArray = {
+  ids: [
+    {
+      hoursID: 'hours-1',
+      minID: 'min-1',
+      secID: 'sec-1',
+      addBtnID: 'add-1',
+      subBtnID: 'sub-1',
+    },
+    {
+      hoursID: 'hours-2',
+      minID: 'min-2',
+      secID: 'sec-2',
+      addBtnID: 'add-2',
+      subBtnID: 'sub-2',
+    },
+  ],
+};
+let i = 2;
+// Document Ready Handler
+$(() => {
+  //  get the template string from the script tag
+  const tplString = $('#tpl_string').html();
+  //  render the template string with the data
+  const tplOutput = Mustache.render(tplString, timeInputArray);
+  //  put the rendered template string into the placeholder div
+  $('#timeRowPlaceholder').html(tplOutput);
+
+  //  add event listeners to the buttons
+  function addRow() {
+    i += 1;
+    timeInputArray.ids.push({
+      hoursID: `hours-${i}`,
+      minID: `min-${i}`,
+      secID: `sec-${i}`,
+      addBtnID: `add-${i}`,
+      subBtnID: `sub-${i}`,
+    });
+    //  put the rendered template string into the placeholder div
+    $('#timeRowPlaceholder').html(tplOutput);
+  }
+
+  //  add event listeners to the buttons
+  $('#moreTimes').on('click', addRow());
+});
