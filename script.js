4ff076270585eba7271fa08273f4974fb67ff505
NosillaCast <allison@podfeet.com>
2021-11-17 11:56:49 -0800

    Click handler working on plus buttons placeholder console.log triggers correctly on buttons before and after addRow runs

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(() => {
     const tplOutput = Mustache.render(tplString, timeInputArray);
     //  put the rendered template string into the placeholder div
     $('#timeRowPlaceholder').html(tplOutput);
-
-    // for (j = 1; j == timeInputArray.ids.length; j += 1) {
-    //   $(`#add-${j}`).click(function() {
-    //     console.log('add button clicked');
-    //   });
-    // };
-  }
+    // add click functions to 3 and up + buttons
+    for (let j = 1; j < (timeInputArray.ids.length + 1); j += 1) {
+      $(`#add-${j}`).click(function() {
+        console.log(`add button add-${j} was clicked`);
+      });
+    };
+  };
   renderRow();
 
 
@@ -45,6 +45,7 @@ $(() => {
   //  add event listeners to the buttons
   function addRow() {
     i += 1;
+    console.log(`DEBUG: i is ${i}`)
     timeInputArray.ids.push({
       hoursID: `hours-${i}`,
       minID: `min-${i}`,
@@ -63,12 +64,12 @@ $(() => {
   // Let's take a crack at doing something when +/- buttons are clicked
   //  add click handler to the +/- buttons
   
-  for (j = 0; j == timeInputArray.ids.length; j += 1) {
-    console.log(`waffles`)
-    $(`#add-${j}`).click(function() {
-      console.log('add button clicked');
-    });
-  };
+  // for (j = 0; j == timeInputArray.ids.length; j += 1) {
+  //   console.log(`waffles`)
+  //   $(`#add-${j}`).click(function() {
+  //     console.log('add button clicked');
+  //   });
+  // };
 
 
 
