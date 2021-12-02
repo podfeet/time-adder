7f062a0faf84984ad2b11da550aaf146f156e749
Allison Sheridan <allison@podfeet.com>
2021-12-02 08:01:49 -0800

    added input variables to timeMath

--- timeMath.mjs
+++ timeMath.mjs
@@ -6,11 +6,10 @@
  * A function to add and subtract elapsed time and return the total
  * @example
  * Given inputs of hVal, mVal and sVal, the function will return the total number of seconds
- * @function timeMath.add
+ * @function timeMath
  */
 
-export function timeMath() {
-  
+export function timeMath(hVal, mVal, sVal) {
   rowTotalSec = hVal*3600+mVal*60+sVal*1;
   totalSec = totalSec + rowTotalSec;
   console.log(`DEBUG: totalSec is ${totalSec}`)
