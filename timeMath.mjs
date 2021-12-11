97efb3a705eb464df499e485f35ba673e91ef4af
Allison Sheridan <allison@podfeet.com>
2021-12-11 12:15:00 -0800

    probably wrong export

--- timeMath.mjs
+++ timeMath.mjs
@@ -8,9 +8,11 @@
  * Given inputs of hVal, mVal and sVal, the function will return the total number of seconds
  * @function timeMath
  */
+let x = 3;
+export {x};
 
-export function timeMath(hVal, mVal, sVal) {
-  rowTotalSec = hVal*3600+mVal*60+sVal*1;
-  totalSec = totalSec + rowTotalSec;
-  console.log(`DEBUG: totalSec is ${totalSec}`)
-}
+// export function timeMath(hVal, mVal, sVal) {
+//   rowTotalSec = hVal*3600+mVal*60+sVal*1;
+//   totalSec = totalSec + rowTotalSec;
+//   console.log(`DEBUG: totalSec is ${totalSec}`)
+// }
