0388d92d6461d1478c63af63dd611ff00b1fab20
Allison Sheridan <allison@podfeet.com>
2022-01-02 08:10:39 -0800

    Switched to Google style guide, fixed most lint errors

--- src/script.js
+++ src/script.js
@@ -1,21 +1,11 @@
-/* eslint-disable consistent-return */
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-
-// Without this I can't have my render function inside the Class
-/* eslint-disable class-methods-use-this */
-
-// without next line, can't add event listener to the input box
-/* eslint-disable no-use-before-define */
-
 /* eslint-env jquery */
 /* global Mustache */
 
-// BUG: can't seem to get the array to show up in the docs
 /**
  * timeInputObject is an Object to hold an array of the IDs for the input boxes for hours, minutes and seconds in each row, along with the add/subtraction button IDs.
- * @typedef {Object} timeInput
- * @property {Array.<{hoursID: string, minID: string, secID: string, addBtnID: string, subBtnID: string}>} ids
+ * 
+ * @typedef {object} timeInput
+ * @property {Array.<{hoursID: string, minID: string, secID: string, addBtnID: string, subBtnID: string}>} ids - the HTML IDs for the input boxes for hours, minutes and seconds in each row, along with the add/subtraction button IDs
  * @param {{hoursID: string, minID: string, secID: string, addBtnID: string, subBtnID: string}} timeInput.ids
  */
 const timeInputObject = {
@@ -75,11 +65,10 @@ $(() => {
     // ****************************** //
     /**
      * @instance
-     * @param {string} tplString - the template string
-     * @param {timeInput}
-     * @param {string} tplOutput - the rendered template string
+     * @param {string} tplString - the template string from the script tag
+     * @property {string} tplOutput - the rendered template string with the data
+     * @param {timeInput} timeInput
      */
-
     renderRow() {
       //  get the template string from the script tag
       const tplString = $('#tpl_string').html();
@@ -124,6 +113,7 @@ $(() => {
 
   /**
    * addRow creates a new row of input boxes and add/subtract buttons. It is triggered by the onclick event handler for the Add Another Row button
+   * 
    * @function addRow
    * @param {number} rowNum - The number of rows before the add a new row button was clicked
    * @returns {number} rowNum - The number of rows after the add a new row button was clicked
@@ -166,37 +156,29 @@ const rowTotalArray = [];
  * Finally the math of this whole project occurs. For the given row of the loop, it calculates the total number of seconds in the row and stores it in the array rowTotalArray at position 'i' of the loop.
  *
  * It then uses the `Array.prototype.reduce()` method to add up all of the values in rowTotalArray and saves it to the constant totSec. totSec then needs to be parsed back into hours, minutes and seconds. calcTime  uses `Math.floor()` to round the value down to the nearest whole number while dividing by 3600, 60, and 1 respectively.
- * 
+ *
  * To do: allow support for subtraction
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce Mozilla docs on reduce()}
  * @function calcTime
- * @param {timeInput}
- * @returns {number} totSec - the total number of seconds in all input boxes
+ * @param {timeInput} timeInput
  */
-
 function calcTime() {
   for (let i = 0; i < rowNum; i += 1) {
-    const { hoursID, minID, secID } = timeInputObject.ids[i];
+    const {hoursID, minID, secID} = timeInputObject.ids[i];
 
     const hVal = Number($(`#${hoursID}`).val());
     const mVal = Number($(`#${minID}`).val());
     const sVal = Number($(`#${secID}`).val());
 
     rowTotalArray[i] = hVal * 3600 + mVal * 60 + sVal;
-    // console.log(`DEBUG: rowTotSec is ${rowTotSec} for row ${i}`);
-    // totSec += rowTotSec;
-    // console.log(`DEBUG: Total Seconds for all rows is ${totSec}`);
-
+    // eslint-disable-next-line max-len
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
     const totSec = rowTotalArray.reduce(reducer);
     // console.log(`DEBUG: Total Seconds for all rows is ${totSec}`);
     const roundHours = Math.floor(totSec / 3600);
-    console.log(`DEBUG: totHours is ${roundHours}`);
     const roundMin = Math.floor((totSec - (roundHours * 3600)) / 60);
-    // console.log(`DEBUG: totMin is ${roundMin}`);
     const roundSec = Math.floor(totSec - (roundHours * 3600) - (roundMin * 60));
-    // console.log(`DEBUG: totSec is ${roundSec}`);
     $('#hTot').html(roundHours);
     $('#mTot').html(roundMin);
     $('#sTot').html(roundSec);
