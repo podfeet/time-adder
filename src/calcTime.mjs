ad2e673018ab91977f75607575b63bad4c9a5fe7
NosillaCast <allison@podfeet.com>
2022-06-07 15:56:59 -0700

    editing text of JSDocs

--- src/calcTime.mjs
+++ src/calcTime.mjs
@@ -6,9 +6,9 @@
  * calcTime calculates the total number of seconds from all of the rows and returns an array of the total hours/min/seconds
  * The function calcTime is triggered by the onchange event of the input fields.
  * Since there's no form submittal, data validation needs to also be triggered by the oninput event of the input fields. The function will check the data as entered for h, m, s and if invalid, add the is-invalid class which will display the error message. If valid it will remove the is-invalid class and erase the error message (or never display it in the first place)
-  * It loops through the ids of all of the input boxes, and on each pass creates an Object with the IDs of the hours, minutes and seconds boxes.  It then finds the value in each input box of the current row and coerces it into a Number and stores the values in constants.
-  * Finally the math of this whole project occurs. For the given row of the loop, it calculates the total number of seconds in the row and stores it in the array rowTotalArray at position 'i' of the loop.
-  * It then uses the `Array.prototype.reduce()` method to add up all of the values in rowTotalArray and saves it to the constant totSec. totSec then needs to be parsed back into hours, minutes and seconds. calcTime  uses `Math.floor()` to round the value down to the nearest whole number while dividing by 3600 for hours, and 60 for minutes. leftoverSec is the remaining seconds in floating point form.
+ * It loops through the ids of all of the input boxes, and on each pass creates an Object with the IDs of the hours, minutes and seconds boxes.  It then finds the value in each input box of the current row and coerces it into a Number and stores the values in constants.
+ * Finally the math of this whole project occurs. For the given row of the loop, it calculates the total number of seconds in the row and stores it in the array rowTotalArray at position 'i' of the loop.
+ * It then uses the `Array.prototype.reduce()` method to add up all of the values in rowTotalArray and saves it to the constant totSec. totSec then needs to be parsed back into hours, minutes and seconds. calcTime  uses `Math.floor()` to round the value down to the nearest whole number while dividing by 3600 for hours, and 60 for minutes. leftoverSec is the remaining seconds in floating point form.
  * 
  * @module calcTime
  * @param {number} rowNum - integer total number of rows
@@ -55,7 +55,6 @@ export default function calcTime(rowNum) {
     const sVal = Number($s.val());
     const rowName = $n.val();
 
-   
     if (rows[id]) {
       (rows.splice(id, 1, [rowName, hVal, mVal, sVal]));
     } else {
