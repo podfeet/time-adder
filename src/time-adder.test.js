670beff62f5a01f49fbf07a4c1025e383d4f727a
Allison Sheridan <allison@podfeet.com>
2022-09-19 20:14:11 -0700

    Created test file for Jest
    doesn't work properly because $ function doesn't load

---
+++ src/time-adder.test.js
@@ -0,0 +1,15 @@
+// This test file doesn't work because jest does not recognize $
+// jQuery isn't loaded because this file looks just at the script.js file
+// and jQuery is called by the html file
+
+const totSect = require('./script');
+
+test ('roundHours if totSec is positive', () => {
+  const totSec = 26760;
+  expect(Math.floor(totSec / 2600).toBe(7));
+});
+
+test ('roundHours if totSec is negative', () => {
+  const totSec = -26760;
+  expect(Math.floor(totSec / 2600).toBe(7));
+});
