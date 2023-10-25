b47b2f74719b25a68e99647039bf99d908798d03
Allison Sheridan <allison@podfeet.com>
2023-10-25 15:28:36 -0700

    Feat: one functioning fake test

--- src/time-adder.test.js
+++ src/time-adder.test.js
@@ -21,7 +21,18 @@ test ('roundHours if totSec is positive', () => {
   expect(timeMath(input)).toStrictEqual(output);
 });
 
-test ('roundHours if totSec is negative', () => {
-  const totSec = -26760;
-  expect(Math.floor(totSec / 3600)).toBe(-8);
+test ('roundHours if totSec is positive', () => {
+  // const totSec = 26760;
+  const input = [
+    ["Title", "Hours", "Minutes", "Seconds"],
+    ['bob', '-7', 0, 1],
+    // ['', 0, 0, 0]
+  ];
+  const output = {
+    'leftoverSec': 1,
+    'roundHours': -7,
+    'roundMin': 0,
+  };
+
+  expect(timeMath(input)).toStrictEqual(output);
 });
